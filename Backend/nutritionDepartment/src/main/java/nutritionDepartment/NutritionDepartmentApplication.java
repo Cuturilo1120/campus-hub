aeba0a7991d9e424d3cd225dc8e1bbcd7ff08c18
Cuturilo1120 <markocuturilo001@gmail.com>
2025-09-17 12:59:55 +0200

    Initiated Nutrition Department service

---
+++ Backend/nutritionDepartment/src/main/java/nutritionDepartment/NutritionDepartmentApplication.java
@@ -0,0 +1,13 @@
+package nutritionDepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionDepartmentApplication.class, args);
+	}
+
+}
---
+++ Backend/nutritionDepartment/src/test/java/nutritionDepartment/NutritionDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nutritionDepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
